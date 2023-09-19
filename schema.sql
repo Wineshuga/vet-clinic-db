ceec0648748dcdc04a3c8c89d7c6d40fb000c6f1
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-19 23:22:25 +0100

    Add database schema to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE
+    animals(
+        id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(50) NOT NULL,
+        date_of_birth DATE NOT NULL,
+        escape_attempts int NOT NULL,
+        neutered BOOLEAN NOT NULL,
+        weight_kg DECIMAL(5, 2) NOT NULL
+    );
