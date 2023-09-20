51669c70ecdccc38a333cf0bf893c8a0dace53db
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-20 14:01:35 +0100

    Add transactions that update table data

--- queries.sql
+++ queries.sql
@@ -22,3 +22,53 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3 
+
+/*First transaction */
+
+BEGIN TRANSACTION;
+
+UPDATE animals set species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+/*Second transaction */
+
+BEGIN;
+
+UPDATE animals set species = 'digimon' WHERE name like '%mon';
+
+UPDATE animals set species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+
+COMMIT;
+
+/*Third transaction */
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals 
+
+/*Fourth transaction */
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SP;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+/*Fifth transaction */
