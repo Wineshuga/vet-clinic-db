58e2a81064c022f139f54f76961bc41df511b713
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-21 17:50:29 +0100

    Add vets data into vets table

--- data.sql
+++ data.sql
@@ -171,7 +171,8 @@ UPDATE animals as a
 SET owner_id = (
         SELECT id
         FROM owners
-        WHERE full_name = 'Bob'
+        WHERE
+            full_name = 'Bob'
     )
 WHERE
     a.name IN ('Devimon', 'Plantmon');
@@ -205,3 +206,25 @@ SELECT name, owner_id from animals;
 SELECT * FROM owners;
 
 COMMIT;
+
+/*Add vets data*/
+
+INSERT INTO
+    vets(name, age, date_of_graduation)
+VALUES (
+        'William Tatcher',
+        45,
+        '2000-04-23'
+    ), (
+        'Maisy Smith',
+        26,
+        '2019-01-17'
+    ), (
+        'Stephanie Mendez',
+        64,
+        '1981-05-04'
+    ), (
+        'Jack Harkness',
+        38,
+        '2008-06-08'
+    );
