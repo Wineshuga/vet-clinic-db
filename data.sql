67c504d22927301a41a56198e102b0c497e3d208
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-21 11:23:01 +0100

    Insert data into owners and species table

--- data.sql
+++ data.sql
@@ -115,3 +115,13 @@ VALUES (
         true,
         22
     );
+
+/*Add data to owners table*/
+
+INSERT INTO
+    owners(full_name, age)
+VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+/*Add data to species table*/
+
+INSERT INTO species(name) VALUES('Pokemon'), ('Digimon');
